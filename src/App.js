b55dbf195607ca4b06ddb4373849b092e507bbad
irerin07 <counterchord89@gmail.com>
2019-09-21 18:12:20 +0900

    All you need to know about State

--- src/App.js
+++ src/App.js
@@ -1,69 +1,21 @@
 import React from 'react';
-import Proptypes from "prop-types"
 
-function Food({name, picture, rating}){
-  return (
-  <div>
-    <h1>I love {name}</h1>
-    <h3>{rating}/5</h3>
-    <img src= {picture} alt={name}/>
-    </div>
-  )
-}
-
-Food.propTypes = {
-  name: Proptypes.string.isRequired,
-  picture: Proptypes.string.isRequired,
-  rating: Proptypes.number
-};
-
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image: 
-    "https://hackernoon.com/hn-images/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-    "https://hackernoon.com/hn-images/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    rating: 3
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-    "https://hackernoon.com/hn-images/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    rating: 4
-  },
-  {
-    id: 4,
-    name: "Ramen",
-    image:
-    "https://hackernoon.com/hn-images/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    rating: 2
-      },
-  {
-    id: 5,
-    name: "Hamberger",
-    image:
-    "https://hackernoon.com/hn-images/1*y6C4nSvy2Woe0m7bWEn4BA.png",
-    rating: 1
-  }
-]
-
-
-function App() {
-  return (
+class App extends React.Component{
+  state={
+    count: 0
+  };
+  render(){
+    return (
     <div>
-      {foodILike.map(dish => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-        ))}
-    </div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
+      </div>
     );
   }
+  add = () => {
+    this.setState(current => ({count: current.count +1}))};
+  minus = () => {this.setState(current => ({count: current.count -1}))};
+}
 
 export default App;
