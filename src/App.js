95b5a1a20a10aca2098da0fafdc0434caef06f0e
irerin07 <counterchord89@gmail.com>
2019-09-24 00:18:36 +0900

    Styling the Movies

--- src/App.js
+++ src/App.js
@@ -1,35 +1,51 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     isLoading: true,
     movies: []
   };
   getMovies = async () => {
-    const {data: {data: {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading: false})
-  }
-  componentDidMount(){
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
     this.getMovies();
   }
   render() {
-    const {isLoading, movies} = this.state;
-    return <div>{isLoading ? "Loading..." 
-    : movies.map(movie=> 
-      <Movie 
-      key={movie.id}
-      id={movie.id} 
-      year={movie.year} 
-      title={movie.title} 
-      summary={movie.summary} 
-      poster={movie.medium_cover_image} 
-      genre={movie.genre}
-      />
-    )
-    }
-    </div>
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genre={movie.genre}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
   // constructor(props){
   //   super(props);
--- src/Movie.js
+++ src/Movie.js
@@ -1,30 +1,32 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 //when the component does not need state, than the component,
 //does not neccessarily have to be class component
 
-function Movie({id, year, title, summary, poster, genre}){
-    return (
-    <div>
-        <h3>ID: {id}</h3>
-        <h3>Released Year: {year}</h3>
-        <h4>Title: {title}</h4>
-        <h4>Summary: {summary}</h4>
-        <img src={poster} alt={title}></img>
-        <h6>Genre: {genre}</h6>
+function Movie({ id, year, title, summary, poster, genre }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div className="movie__data">
+        <h3 className="movie__id">{id}</h3>
+        <h3 className="movie__title">{title}</h3>
+        <h3 className="movie__year">{year}</h3>
+        <p className="movie__summary">{summary}</p>
+        <h3 className="movie__genre">{genre}</h3>
+      </div>
     </div>
-    )
-    
+  );
 }
 
 Movie.propTypes = {
-    id: PropTypes.number.isRequired,
-    year:PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    summary: PropTypes.string,
-    poster: PropTypes.string.isRequired,
-    genre: PropTypes.array
-}
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string,
+  poster: PropTypes.string.isRequired,
+  genre: PropTypes.array
+};
 
 export default Movie;
